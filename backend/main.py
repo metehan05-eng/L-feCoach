43bd21845e1655db83d042c08b0e92746fedddee
metehan05-eng <metehan05-eng@example.com>
2025-12-17 17:39:45 +0300

    Remove reload for production and use HOST env var

--- backend/main.py
+++ backend/main.py
@@ -55,4 +55,5 @@ if __name__ == "__main__":
     import uvicorn
     import os
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    host = os.getenv("HOST", "0.0.0.0")
+    uvicorn.run("main:app", host=host, port=port)
